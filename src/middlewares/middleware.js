840cfebdbdf30d4ca76ed4da4e586121e4985447
larissaagabrielle <103789688+larissaagabrielle@users.noreply.github.com>
2023-08-13 14:16:30 -0300

    Finaliza a página de login

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -8,9 +8,11 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err && 'EBADCSRFTOKEN' === err.code) {
+  if(err) {
     return res.render('404');
+    // renderiza a página 404 se houver um erro no csrf token
   }
+  next() ;// passa para o próximo middleware ou rota
 };
 
 exports.csrfMiddleware = (req, res, next) => {
